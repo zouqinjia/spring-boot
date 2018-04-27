6848a8f2f5fbc86db48c18da0d7ba2bff74cc757
zouqinjia <zoujiaqin@re-seek.com>
2018-04-27 17:05:02 +0800

    静态资源加载和视图解析器配置

--- chapter4/src/main/java/com/example/demo/configuration/MVCConfig.java
+++ chapter4/src/main/java/com/example/demo/configuration/MVCConfig.java
@@ -1,6 +1,7 @@
 package com.example.demo.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceView;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
  * Description:
  * Date:2018/4/27 15:37
  **/
+@Configuration
 public class MVCConfig implements WebMvcConfigurer {
 
 //    @Bean
@@ -27,8 +29,8 @@ public class MVCConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-        registry.addResourceHandler("/static/**")// 为基于指定的URL路径模式的静态资源添加一个资源处理程序。
-                .addResourceLocations("classpath:/static/**");// 为静态内容添加一个或多个资源位置。
+        registry.addResourceHandler("/static/**")// 对外暴露的访问路径
+                .addResourceLocations("classpath:/static/**");// 文件放置目录
     }
 
     /**
